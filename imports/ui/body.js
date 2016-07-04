bb09cd3c14fc21c2b2d7fd76774963d1d0d8ebf6
Chris Sheehan <chris.sheehan@ymail.com>
2016-07-04 14:31:21 +0100

    local state added to enable hiding of completed tasks

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,25 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js'
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
     tasks() {
-      return Tasks.find({}, { sort: { createdAt: -1 } });
+      const instance = Template.instance();
+      if (instance.state.get('hideCompleted')) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, { sort: { createdAt: -1 } });
+      }
     }
 });
 
@@ -22,8 +34,10 @@ Template.body.events({
       text,
       createdAt: new Date(),
     });
+    // Clear form
     target.text.value = '';
-
   },
-
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
+  },
 })
